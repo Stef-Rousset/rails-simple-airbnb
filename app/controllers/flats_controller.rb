12042d6770fa59ac5926f71b384b9356160f3b3b
stephanie rousset <strabon@club-internet.fr>
2020-08-16 12:18:29 +0200

    working on airbnb

---
+++ app/controllers/flats_controller.rb
@@ -0,0 +1,51 @@
+class FlatsController < ApplicationController
+  before_action :set_flats, only: [:show, :edit, :update, :delete]
+
+  def index
+    if params[:query].present?
+      @query = params[:query]
+      @flats = Flat.where("name iLike '%#{@query}%'")
+    else
+      @flats = Flat.all
+    end
+  end
+
+  def show
+  end
+
+  def new
+    @flat = Flat.new
+  end
+
+  def create
+    @flat = Flat.new(flat_params)
+    if @flat.save
+      redirect_to flats_path
+    else
+      render :new
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    @flat.update(flat_params)
+    redirect_to flat_path(@flat)
+  end
+
+  def destroy
+    @flat.destroy
+    redirect_to flats_path
+  end
+
+  private
+
+  def set_flats
+    @flat = Flat.find(params[:id])
+  end
+
+  def flat_params
+    params.require(:flat).permit(:name, :address, :description, :price_per_night, :number_of_guests)
+  end
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+
+puts "creating 3 flats"
+3.times do
+  flat = Flat.new(
+    name: Faker::Company.name,
+    address: Faker::Address.full_address,
+    description: Faker::Lorem.sentence,
+    price_per_night: rand(50..100),
+    number_of_guests: rand(0..5)
+    )
+  flat.save!
+end
+
+puts "done"
